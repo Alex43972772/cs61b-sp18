d00fcc436498766470095949ac1a83b69a4fe469
Alex43972772 <zcx1026187685@gmail.com>
2024-03-16 14:09:21 +0800

    first try for lab1 leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,14 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Alex Zheng
  */
 public class LeapYear {
 
+    /** Decide whether the given year is a leapyear or not.
+     */
+     private static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0 )) return true;
+        return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
