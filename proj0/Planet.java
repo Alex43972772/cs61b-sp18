2446bd8c3ef351866d0615ab5d698e1a018c2444
Alex43972772 <zcx1026187685@gmail.com>
2024-03-19 20:57:17 +0800

    finish Planet class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -22,6 +22,53 @@ public class Planet {
         mass = p.mass;
         imgFileName = p.imgFileName;
     }
+    public double calcDistance(Planet p) {
+        double dx = this.xxPos - p.xxPos;
+        double dy = this.yyPos - p.yyPos;
+        double r2 = dx * dx + dy * dy;
+        return Math.sqrt(r2);
+    }
+    public double calcForceExertedBy(Planet p) {
+        double G = 6.67 * 0.00000000001;
+        double r = calcDistance(p);
+        return G * this.mass * p.mass / (r * r);
+    }
+    public double calcForceExertedByX(Planet p) {
+        double dx = p.xxPos - this.xxPos;
+        double r = calcDistance(p);
+        double f = calcForceExertedBy(p);
+        return f * dx / r;
+    }
+    public double calcForceExertedByY(Planet p) {
+        double dy = p.yyPos - this.yyPos;
+        double r = calcDistance(p);
+        double f = calcForceExertedBy(p);
+        return f * dy / r;
+    }
+    public double calcNetForceExertedByX(Planet[] planets){
+        double nf = 0;
+        for (Planet p : planets) {
+            if (this.equals(p)) continue;
+            nf += calcForceExertedByX(p);
+        }
+        return nf;
+    }
+    public double calcNetForceExertedByY(Planet[] planets){
+        double nf = 0;
+        for (Planet p : planets) {
+            if (this.equals(p)) continue;
+            nf += calcForceExertedByY(p);
+        }
+        return nf;
+    }
+    public void update(double dt, double fx, double fy) {
+        double ax = fx / mass;
+        double ay = fy / mass;
+        xxVel += dt * ax;
+        yyVel += dt * ay;
+        xxPos += dt * xxVel;
+        yyPos += dt * yyVel;
+    }
 }
 
 
