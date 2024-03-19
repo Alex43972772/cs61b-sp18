782850fbe1d739ce12103f1e6cfea75cde7c726c
Alex43972772 <zcx1026187685@gmail.com>
2024-03-19 21:22:58 +0800

    finish more methods

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,6 +1,24 @@
 public class NBody {
-    public double readRadius(String filename) {
+    public static double readRadius(String filename) {
         In in = new In(filename);
+        int planets = in.readInt();
         return in.readDouble();
+    }
+    public static Planet[] readPlanets(String filename) {
+        In in = new In(filename);
+        int n = in.readInt();
+        double radius = in.readDouble();
+        Planet[] res = new Planet[n];
+        for (int i = 0; i < n; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            Planet added = new Planet(xP, yP, xV, yV, m, img);
+            res[i] = added;
+        }
+        return res;
     }
 }
